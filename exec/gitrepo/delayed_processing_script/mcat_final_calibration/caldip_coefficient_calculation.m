4f63e308248a35ac5af881518ee4046cec75a36d
sjones1000 <samj__@hotmail.com>
2020-12-03 14:37:41 +0000

    Updated dy120 caldip pressure intervals

--- exec/gitrepo/delayed_processing_script/mcat_final_calibration/caldip_coefficient_calculation.m
+++ exec/gitrepo/delayed_processing_script/mcat_final_calibration/caldip_coefficient_calculation.m
@@ -12,8 +12,8 @@ close all
 
 %========================================================================
 % Calculation of calibration coefficient
-p_insitucal.cruise           =  'dy120';%'dy078';%'dy053';%'pe400'; %'kn221-02'; %'pe399';       % campaign cd177 / cd170 / d304 / kn182, ...
-p_insitucal.cast             = 10;  %26;   %35          % number of ctd - cast (e.g. kn221-02: 4, 5)
+p_insitucal.cruise           =  'ar304'; %'dy120' %'ar304' %'dy078';%'dy053';%'pe400'; %'kn221-02'; %'pe399';       % campaign cd177 / cd170 / d304 / kn182, ...
+p_insitucal.cast             = 45;  %26;   %35          % number of ctd - cast (e.g. kn221-02: 4, 5)
 p_insitucal.depl_period      = 'osnap5'; %'osnap2'    % move1; move2; rapid 1; rapid 2
 
 % ---- parameters ----------------------------------------------------
