ef44d3c22b40841e95364c71de154e7ab6d41bb2
LewisDrysdale <lewisdrysdale@hotmail.co.uk>
2021-02-01 15:15:45 +0000

    update calib scripts

--- exec/gitrepo/delayed_processing_script/mcat_final_calibration/caldip_coefficient_calculation.m
+++ exec/gitrepo/delayed_processing_script/mcat_final_calibration/caldip_coefficient_calculation.m
@@ -15,7 +15,7 @@ close all
 % Calculation of calibration coefficient
 % For cruise dy120: cast= [1,3,4,5,8,9,10]
 p_insitucal.cruise           =  'dy120' ;%'ar304'; %'ar304' %'dy078';%'dy053';%'pe400'; %'kn221-02'; %'pe399';       % campaign cd177 / cd170 / d304 / kn182, ...
-p_insitucal.cast             = 3;  
+p_insitucal.cast             = 4;  
 p_insitucal.depl_period      = 'osnap5'; %'osnap2'    % move1; move2; rapid 1; rapid 2
 
 % ---- parameters ----------------------------------------------------
