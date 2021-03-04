fabf8769ba283dbab2373e6a884e697d6a85fe31
LewisDrysdale <lewisdrysdale@hotmail.co.uk>
2021-03-04 13:13:27 +0000

    de-bug IB-moor

--- exec/gitrepo/delayed_processing_script/mcat_final_calibration/microcat_apply_cal_plus_rbr_idr_osnap.m
+++ exec/gitrepo/delayed_processing_script/mcat_final_calibration/microcat_apply_cal_plus_rbr_idr_osnap.m
@@ -17,13 +17,9 @@ clearvars  -except pathosnap pathgit
 warning off
 
 % path of the mooring data define in the startup file under osnap/
-<<<<<<< HEAD
 %moor = 'rtwb1_05_2018';
-moor = 'ib5_01_2018';
-=======
-moor = 'rteb1_05_2018';
-% moor = 'ib3_01_2018';
->>>>>>> postdy120
+% moor = 'ib5_01_2018';
+moor = 'ib3_01_2018';
 
 %=========================================================================
 % Apply calibration coefficients to series, removes bad data. If required, applies
