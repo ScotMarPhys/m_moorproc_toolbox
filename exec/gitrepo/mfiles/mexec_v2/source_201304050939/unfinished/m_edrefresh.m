0226e37b290c987d9ac485cb3c46db90793bc5c3
Yvonne Firing <yvonne.firing@noc.ac.uk>
2024-06-18 10:21:47 +0100

    moved external toolboxes to other_software

--- exec/gitrepo/mfiles/mexec_v2/source_201304050939/unfinished/m_edrefresh.m
+++
@@ -1,7 +0,0 @@
-function pdfot = m_refresh(pdfin)
-
-close
-pdfin.ncfile.noflagcheck = 1;
-pdfot = m_edplot(pdfin);
-return
-
--- exec/gitrepo/mfiles/mexec_v2/source_201304050939/unfinished/m_remove_spaces.m
+++
@@ -1,9 +0,0 @@
-function sot = m_remove_spaces(sin)
-
-% remove any spaces from a string
-
-s = sin;
-k = strfind(sin,' ');
-s(k) = [];
-sot = s;
-return
