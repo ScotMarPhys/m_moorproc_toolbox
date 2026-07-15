18a451610b67fa0cdc8976d0455f511b4f1406c6
Kristin Burmeister <kristin.burmeister@sams.ac.uk>
2026-07-15 08:40:03 +0100

    moor_inoutpaths: renamed nor stage 2 log conistent with stage 1 and 3 logs

--- code/mfiles/moor_inoutpaths.m
+++ code/mfiles/moor_inoutpaths.m
@@ -82,7 +82,7 @@ switch datatype
         pd.stage1log = fullfile(pd.stage1path, [moor '_Nortek_stage1.log']);
         pd.stage1form = [moor '_%d.raw'];
         pd.stage2path = fullfile(mg.moordatadir, 'proc', moor, datatype);
-        pd.stage2log = fullfile(pd.stage2path, ['stage2_log_Nortek_' moor]);
+        pd.stage2log = fullfile(pd.stage2path, [moor '_Nortek_stage2.log']);
         pd.stage2form = [moor '_%d.use'];
         pd.stage3path = fullfile(mg.moordatadir, 'proc', moor, datatype);
         pd.stage3form = [moor '_%d.edt'];
